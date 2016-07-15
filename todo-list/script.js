15fa2b7b3982ca42fa1a0b63a7eb9cf943c55198
nadinerose <nadinerose2124@gmail.com>
2016-07-14 20:20:09 -0700

    Added array to To Do List

--- todo-list/script.js
+++ todo-list/script.js
@@ -8,12 +8,13 @@ var newTask = document.querySelector("#new-item-input");
 // Click on task and task gets unchecked and uncrossed
 
 function createTask(todo) {
+
   var listItem = document.createElement("li");
   var checkbox = document.createElement("input");
   var label = document.createElement("label");
   var span = document.createElement("span");
 
- checkbox.setAttribute("type", "checkbox");
+ checkbox.setAttribute("type","checkbox");
  span.textContent = todo;
 
  label.appendChild(checkbox);
@@ -25,7 +26,7 @@ function createTask(todo) {
 function formSubmitted(event) {
   event.preventDefault();
 
-    createTask(newTask.value);
+  newTask.value.split(",").forEach(createTask);
 
   form.reset();
 }
