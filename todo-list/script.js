fc3859494c9a9ac07f8f825264fe2f095cb5d60e
nadinerose <nadinerose2124@gmail.com>
2016-07-14 18:21:56 -0700

    Finished code for to-do list

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,19 +1,31 @@
 var form = document.querySelector("#new-item-form");
-var toDoList = document.querySelector ("#todo-list")
+var toDoList = document.querySelector("#todo-list");
+var newTask = document.querySelector("#new-item-input");
+
+// Type in task
+// Press Enter and task is listed
+// Click on task and task is checked off and crossed off
+// Click on task and task gets unchecked and uncrossed
+
+function createTask(todo) {
+  var listItem = document.createElement("li");
+  var checkbox = document.createElement("input");
+  var label = document.createElement("label");
+  var span = document.createElement("span");
+
+ checkbox.setAttribute("type", "checkbox");
+ span.textContent = todo;
+
+ label.appendChild(checkbox);
+ label.appendChild(span);
+ listItem.appendChild(label);
+ toDoList.appendChild(listItem);
+}
 
 function formSubmitted(event) {
   event.preventDefault();
 
-  // Your code goes here...
-  // Type in task
-  // Press Enter and task is listed
-  // Click on task and task is checked off and crossed off
-  // Click on task and task gets unchecked and uncrossed
-
-    var newTask = document.createElement("div");
-    form.textContent = toDoList.value;
-    toDoList.classList.add ("form");
-    buttonish.appendChild(form);
+    createTask(newTask.value);
 
   form.reset();
 }
