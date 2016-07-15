c72c9473b92745e646af47d969feae8f01b70e7f
nadinerose <nadinerose2124@gmail.com>
2016-07-14 17:55:30 -0700

    Typed out process and started adding JS code

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,9 +1,19 @@
 var form = document.querySelector("#new-item-form");
+var toDoList = document.querySelector ("#todo-list")
 
 function formSubmitted(event) {
   event.preventDefault();
 
   // Your code goes here...
+  // Type in task
+  // Press Enter and task is listed
+  // Click on task and task is checked off and crossed off
+  // Click on task and task gets unchecked and uncrossed
+
+    var newTask = document.createElement("div");
+    form.textContent = toDoList.value;
+    toDoList.classList.add ("form");
+    buttonish.appendChild(form);
 
   form.reset();
 }
